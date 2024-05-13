7cb704355dcfad6f35059847c3941a6eb0a7727b
carlos1510 <carloscle1510@gmail.com>
2024-05-13 08:54:17 -0500

    Agregar modificacion en configuracion de ruta

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const URL_BASE =
-  "http://161.132.38.201/api_creditoprendario/public/api";
+  "http://127.0.0.1/api_creditoprendario/public/api";
 
 export const tokenKey = "boardable-auth-token";
