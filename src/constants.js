164d0be5ec516686be10f522a24a7a1e8605b1ad
carlos1510 <carloscle1510@gmail.com>
2024-05-13 09:02:35 -0500

    nueva modificacion en el package.json

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const URL_BASE =
-  "http://127.0.0.1/api_creditoprendario/public/api";
+  "http://localhost/api_creditoprendario/public/api";
 
 export const tokenKey = "boardable-auth-token";
