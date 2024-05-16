ce54bb601af6d6275d7096faf752caf77390aefd
carlos1510 <carloscle1510@gmail.com>
2024-05-16 12:46:01 -0500

    agregar modificacion en el constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-//export const URL_BASE = "http://161.132.38.201/api_creditoprendario/public/api";
-export const URL_BASE = "http://127.0.0.1:8000/api";
+export const URL_BASE = "http://161.132.38.201/api_creditoprendario/public/api";
+//export const URL_BASE = "http://127.0.0.1:8000/api"
 
 export const tokenKey = "boardable-auth-token";
