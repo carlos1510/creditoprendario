be9a9bcd5c182ce51f48f1d5c759fd5af30a0b17
carlos1510 <carloscle1510@gmail.com>
2024-05-31 12:37:45 -0500

    Agregar constants

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,5 @@ export const URL_BASE = "http://161.132.38.201/api_creditoprendario/public/api";
 //export const URL_BASE = "http://127.0.0.1:8000/api"
 
 export const tokenKey = "boardable-auth-token";
+export const userName = "username";
+export const rol = "rol";
